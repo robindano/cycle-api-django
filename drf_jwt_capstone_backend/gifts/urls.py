2af9f97f22b0ca69c47fe93f003e0df19fe0d868
robindano <151894372+robindano@users.noreply.github.com>
2021-12-19 17:38:49 -0600

    Created GiftDetail views

--- drf_jwt_capstone_backend/gifts/urls.py
+++ drf_jwt_capstone_backend/gifts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from gifts import views
 
 urlpatterns = [
-    path('', views.GiftList.as_view())
+    path('', views.GiftList.as_view()),
+    path('<int:pk>/', views.GiftDetail.as_view())
 ]
--- drf_jwt_capstone_backend/gifts/views.py
+++ drf_jwt_capstone_backend/gifts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http.response import Http404
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Gift
@@ -23,3 +24,35 @@ class GiftList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GiftDetail(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Gift.objects.get(pk=pk)
+        except Gift.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        gift = self.get_object(pk)
+        serializer = GiftSerializer(gift)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        gift = self.get_object(pk)
+        if gift.giver.id == request.user.id:
+            serializer = GiftSerializer(gift, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    def delete(self, request, pk):
+        gift = self.get_object(pk)
+        if gift.giver.id == request.user.id:
+            gift.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
