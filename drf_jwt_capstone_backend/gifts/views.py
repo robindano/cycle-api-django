7eb49c14a89e770ac2facf10b94f186e1e128d07
robindano <151894372+robindano@users.noreply.github.com>
2022-01-03 16:16:22 -0600

    Fixed pick_winner task (for now)

--- drf_jwt_capstone_backend/gifts/views.py
+++ drf_jwt_capstone_backend/gifts/views.py
@@ -7,7 +7,8 @@ from rest_framework.decorators import api_view, permission_classes
 from .models import Gift
 from .tasks import pick_winner, print_expiration
 from .serializers import GiftSerializer
-import random
+from datetime import datetime, timedelta
+from django.utils import timezone
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -24,9 +25,10 @@ class GiftList(APIView):
         serializer = GiftSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            #celery task
-            print_expiration.apply_async(countdown=3)
-            pick_winner.apply_async((serializer.id), eta=serializer.expiration)
+            print(serializer.data['expiration'])
+            # expiration = timezone.now() + timedelta(hours=1)
+            # celery task
+            pick_winner.apply_async([serializer.data['id']], eta=serializer.data['expiration'])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
